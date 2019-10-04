8db8aa02e15ed325bcda94f21acee86ad809a780
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-10-04 16:15:47 +0200

    Replace RecyclerView with component list with plain text in About section

--- app/src/main/java/com/brittlepins/brittleeye/AboutActivity.kt
+++ app/src/main/java/com/brittlepins/brittleeye/AboutActivity.kt
@@ -1,13 +1,9 @@
 package com.brittlepins.brittleeye
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_about.*
-import kotlinx.android.synthetic.main.content_about.*
 
 class AboutActivity : AppCompatActivity() {
 
@@ -20,21 +16,5 @@ class AboutActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        val components = arrayListOf<Pair<Drawable?, String>>()
-        components.add(Pair(getDrawable(R.drawable.analogue_input_output), getString(R.string.analogue_input_output)))
-        components.add(Pair(getDrawable(R.drawable.audio_coder_decoder), getString(R.string.ausio_coder_decoder)))
-        components.add(Pair(getDrawable(R.drawable.bluetooth_module), getString(R.string.bluetooth_module)))
-        components.add(Pair(getDrawable(R.drawable.dataflash_storage), getString(R.string.dataflash_storage)))
-        components.add(Pair(getDrawable(R.drawable.flame_sensor), getString(R.string.flame_sensor)))
-        components.add(Pair(getDrawable(R.drawable.heartbeat_sensor), getString(R.string.heartbeat_sensor)))
-        components.add(Pair(getDrawable(R.drawable.reed_switch), getString(R.string.reed_switch)))
-
-        val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        componentList.apply {
-            setHasFixedSize(true)
-            adapter = AboutComponentsAdapter(components)
-            layoutManager = manager
-        }
     }
 }
